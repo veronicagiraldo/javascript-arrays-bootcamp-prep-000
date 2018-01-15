44be504c5ad9e374af481a580145bba6fc3426fa
Veronica Giraldo <veronica9893@gmail.com>
2018-01-15 23:29:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,7 @@ function addElementToBeginningOfArray(array, element){
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+  return array 
 }
 function addElementToEndOfArray(array, element){
   return[element,...array]
