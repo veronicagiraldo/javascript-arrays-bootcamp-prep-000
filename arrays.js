0915176fec137be0611fb4d04325b9b3e29b9d11
Veronica Giraldo <veronica9893@gmail.com>
2018-01-15 23:20:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,5 +25,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array.slice(0,array.length-1)
 }
 function removeElementFromEndOfArray(array) {
-  return array.pop()
+  return array.slice(0,)
 }
