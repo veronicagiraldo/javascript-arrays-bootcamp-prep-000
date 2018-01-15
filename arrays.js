c7b7ce9edb529718e826d121cfaa0b38af518a70
Veronica Giraldo <veronica9893@gmail.com>
2018-01-15 23:16:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,5 +19,6 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
   return unshift [element]
 }
 function removeElementFromBeginningOfArray(array) {
-  return array.slice[element]
+  return array.slice(1)
 }
+function 
